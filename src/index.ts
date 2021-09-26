f068aa9a5bb62a95a39a17f4ba09165582a9d916
Jonathan Miller <jon@jmill.dev>
2021-09-26 16:12:32 -0400

    Changes to route

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,32 @@ import express from "express";
 import { AnnotationResponse } from "./baseAnnotator";
 import helloLinter from "./annotators/helloLint";
 var bodyParser = require("body-parser");
+var cors = require("cors");
 const app = express();
 const port = 3000;
 
-var jsonParser = bodyParser.json();
+app.use(cors());
+
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  next();
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Express + xfdsfd Server"));
 
-app.post("/annotate", jsonParser, (req, res) => {
+app.post("/annotate", (req, res) => {
   // tslint:disable-next-line:no-console
-  console.log(req);
+  console.log(req.headers)
+  console.log(req.body);
   const { text, annotators } = req.body;
   const annotationResponses: AnnotationResponse[] = [];
   for (const annotator of annotators) {
@@ -21,7 +37,6 @@ app.post("/annotate", jsonParser, (req, res) => {
     } else {
       throw new Error(`Unknown annotator: ${annotator}`);
     }
-    console.log("hello");
   }
   res.send(JSON.stringify(annotationResponses));
 });
